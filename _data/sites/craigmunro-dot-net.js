3303be229b4c59fad929121392f190533e56852b
Craig Munro <craig@eurogamer.net>
2024-05-12 15:48:06 +0100

    Update and rename sample.js to craigmunro-dot-net.js

---
+++ _data/sites/craigmunro-dot-net.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "craigmunro.net",
+	description: "craigmunro.net",
+	options: {
+		runs: 1,
+		frequency: 1440, //once per day
+	},
+	urls: [
+		"https://www.craigmunro.net/"
+	]
+};
