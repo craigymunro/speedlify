b2bdbe80e902d6a2706b2d10933ae0963402c319
Craig Munro <craig@eurogamer.net>
2024-05-12 15:52:15 +0100

    Create gamer-network.js

---
+++ _data/sites/gamer-network.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "Gamer Network",
+	description: "Gamer Network's sites",
+	options: {
+		runs: 1,
+		frequency: 1440, //once per day
+	},
+	urls: [
+		"https://www.eurogamer.net/",
+    "https://www.rockpapershotgun.com/",
+    "https://www.vg247.com/",
+    "https://www.gamesindustry.biz/",
+    "https://www.dicebreaker.com/",
+    "https://www.thepopverse.com/"
+  ]
+};
