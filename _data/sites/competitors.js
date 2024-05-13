aa89a38895769d0beb1273a4a12c068d3f4a5580
Craig Munro <craig@eurogamer.net>
2024-05-13 09:06:50 +0100

    Create competitors.js

---
+++ _data/sites/competitors.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "Competitors",
+	description: "Competitor sites",
+	options: {
+		runs: 1,
+		frequency: 1440, //once per day
+	},
+	urls: [
+		"https://www.ign.com/",
+    "https://www.gamespot.com/",
+    "https://www.polygon.com/",
+    "https://www.gameinformer.com/",
+    "https://www.kotaku.com/",
+    "https://www.gamerant.com/"
+  ]
+};
