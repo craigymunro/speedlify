74f010bb82e87db56104c3a22ee35a250c9217ec
Craig Munro <craig@eurogamer.net>
2024-05-24 11:54:30 +0100

    Create ziff-davis.js

---
+++ _data/sites/ziff-davis.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "Ziff Davis",
+	description: "Ziff Davis sites",
+	options: {
+		runs: 1,
+		frequency: 1440, //once per day
+	},
+	urls: [
+		"https://www.ign.com/",
+    "https://mashable.com/",
+    "https://lifehacker.com/",
+    "https://www.askmen.com/",
+    "https://www.pcmag.com/",
+  ]
+};
