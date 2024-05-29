36adef06518f6a41218b3dcc606ef82f85714961
Craig Munro <craig@eurogamer.net>
2024-05-29 16:34:44 +0100

    Create reviews.js

---
+++ _data/sites/reviews.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "GTA 5 reviews",
+	description: "GTA 5 reviews,
+	options: {
+		runs: 1,
+		frequency: 1440, //once per day
+	},
+	urls: [
+    "https://www.eurogamer.net/grand-theft-auto-5-review",
+    "https://www.rockpapershotgun.com/gta-5-pc-review",
+    "https://www.vg247.com/grand-theft-auto-v-review",
+    "https://www.ign.com/articles/2013/09/16/grand-theft-auto-v-review",
+    "https://www.gameinformer.com/2022/03/16/im-playing-grand-theft-auto-v-for-the-first-time-and-it-lives-up-to-the-hype",
+    "https://www.gamespot.com/reviews/grand-theft-auto-5-ps4-xbox-one-pc-review/1900-6415959/",
+    "https://www.pcgamer.com/gta-5-review/"
+  ]
+};
