af684fc41a682cde4c7fed0d0c37a33f408d73ce
Craig Munro <craig@eurogamer.net>
2024-07-17 13:27:58 +0100

    Add Hookshot sites

---
+++ _data/sites/hookshot.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Hookshot",
+  description: "Hookshot sites",
+  options: {
+    runs: 1,
+    frequency: 1440, //once per day
+  },
+  urls: [
+    "https://www.nintendolife.com/",
+    "https://www.pushsquare.com/",
+    "https://www.purexbox.com/",
+    "https://www.timeextension.com/",
+  ]
+};
