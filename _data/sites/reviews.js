f055deee1f9dc27143d6f139cb6b89f04bf586ce
Craig Munro <craig@eurogamer.net>
2024-07-26 14:53:57 +0100

    Update reviews.js

--- _data/sites/reviews.js
+++ _data/sites/reviews.js
@@ -12,6 +12,10 @@ module.exports = {
     "https://www.ign.com/articles/2013/09/16/grand-theft-auto-v-review",
     "https://www.gameinformer.com/games/grand_theft_auto_v/b/xbox360/archive/2013/09/16/grand-theft-auto-v-review.aspx",
     "https://www.gamespot.com/reviews/grand-theft-auto-5-ps4-xbox-one-pc-review/1900-6415959/",
-    "https://www.pcgamer.com/gta-5-review/"
+    "https://www.pcgamer.com/gta-5-review/",
+    "https://www.polygon.com/reviews/23281268/grand-theft-auto-5-re-review-gta-online-playstation-5-xbox-series-x-pc-steam-rockstar-games",
+    "https://www.theguardian.com/technology/2013/sep/16/gta-5-review-grand-theft-auto-v",
+    "https://www.gamesradar.com/gta-5-review/",
+    "https://www.videogamer.com/reviews/20141120172304-grand-theft-auto-5-review/",
   ]
 };
